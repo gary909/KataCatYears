3c0b67ca0e39ccdf61734e890165252d23fa397e
gary <gary.white@lodgit.com>
2021-07-01 16:30:28 +0200

    files

---
+++ catYears.js
@@ -0,0 +1,21 @@
+function catYears(){
+    return "hello";
+}
+
+console.log(catYears())
+console.log("test")
+
+//*******************************/
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    // Your code here!
+    var myArray = [0, 0, 0];
+    humanYears == myArray[0];
+
+
+    //return [0,0,0];
+    return humanYears;
+  }
+
+  console.log(humanYearsCatYearsDogYears(6));
+  
