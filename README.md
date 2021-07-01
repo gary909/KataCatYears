f813056a3bb14a9f236b53b67c4808ed5698c043
gary <gary.white@lodgit.com>
2021-07-01 17:04:55 +0200

    working code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # KataCatYears
+
+About
+From the codewars kata 'Cat years, Dog years'
+
--- catYears.js
+++ catYears.js
@@ -9,13 +9,26 @@ console.log("test")
 
 var humanYearsCatYearsDogYears = function(humanYears) {
     // Your code here!
-    var myArray = [0, 0, 0];
-    humanYears == myArray[0];
+    var myArray = [humanYears, 0, 0];
+
+    if (humanYears == 1) {
+        myArray = [1,15,15];
+    } else if (humanYears == 2) {
+        myArray = [2,24,24];
+    } else {
+        var catYears = 24;
+        myArray[1] = (humanYears -2) * 4 + catYears;
+    
+        var dogYears = 24;
+        myArray[2] = (humanYears -2) * 5 + dogYears;
+    }
 
 
     //return [0,0,0];
-    return humanYears;
+    // return humanYears
+    
+    return myArray;
   }
 
-  console.log(humanYearsCatYearsDogYears(6));
+  console.log(humanYearsCatYearsDogYears(10));
   
